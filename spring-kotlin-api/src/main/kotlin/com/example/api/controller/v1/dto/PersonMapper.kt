a0996d5653c83a4526fe7fb02d37cd1f8b5d89e8
kim_jinyoung_aart <kotko123@naver.com>
2024-08-19 22:22:29 +0900

    MapStruct on spring

--- spring-kotlin-api/src/main/kotlin/com/example/api/controller/v1/dto/PersonMapper.kt
+++ spring-kotlin-api/src/main/kotlin/com/example/api/controller/v1/dto/PersonMapper.kt
@@ -4,13 +4,18 @@ import com.example.core.domain.Person
 import org.mapstruct.InheritInverseConfiguration
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.MappingConstants.ComponentModel.SPRING
 
-@Mapper
+@Mapper(componentModel = SPRING)
 interface PersonMapper {
 
   @Mapping(source = "phoneNumber", target = "phone")
-  fun convertToDto(person: Person) : PersonDto
+  fun convertToDto(person: Person): PersonDto
+
+  @Mapping(source = "phoneNumber", target = "phone")
+  fun convertListToDtos(persons: List<Person>): List<PersonDto>
 
   @InheritInverseConfiguration
-  fun convertToModel(personDto: PersonDto) : Person
+  fun convertToModel(personDto: PersonDto): Person
+  fun convertListToModels(personDtos: List<PersonDto>): List<Person>
 }
--- spring-kotlin-api/src/main/kotlin/com/example/api/service/PersonService.kt
+++ spring-kotlin-api/src/main/kotlin/com/example/api/service/PersonService.kt
@@ -2,23 +2,36 @@ package com.example.api.service
 
 import com.example.api.dto.PersonMapper
 import com.example.core.domain.Person
-import org.mapstruct.factory.Mappers
+import com.example.core.log.logger
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 @Service
-class PersonService {
+class PersonService(
+  private val personMapper: PersonMapper
+) {
+  companion object {
+    private val log = logger()
+  }
 
   fun testConvert() {
-
-    val converter = Mappers.getMapper(PersonMapper::class.java)
+//    val personMapper = Mappers.getMapper(PersonMapper::class.java)
 
     val person = Person("Samuel", "Jackson", "0123 334466", LocalDate.of(1948, 12, 21))
 
-    val personDto = converter.convertToDto(person)
+    val personDto = personMapper.convertToDto(person)
     println(personDto)
 
-    val personModel = converter.convertToModel(personDto)
+    val personModel = personMapper.convertToModel(personDto)
     println(personModel)
+
+    val persons = mutableListOf(person, personModel)
+    log.info { "persons(${persons.size}): $persons" }
+
+    val personDtos = personMapper.convertListToDtos(persons)
+    log.info { "personDtos(${personDtos.size}): $personDtos" }
+
+    val personList = personMapper.convertListToModels(personDtos)
+    log.info { "personList(${personList.size}): $personList" }
   }
 }
